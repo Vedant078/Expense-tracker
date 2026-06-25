d469ced5f07cf25a53578ace2f2ce362d262b5fd
vedant <ved.csbs@gmail.com>
2026-06-25 23:55:44 +0530

    Implementing jwt verificaton for auhentication and oauth2 for authorisation

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,15 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException,status
 from sqlmodel import select, Session
-from passlib.context import CryptContext
 from app.config.database import get_session
 from app.models.schemas import User,UserCreate,UserResponse
+from app.utils.security import hashPassword,verify_password,create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
+
+
 
 router = APIRouter(prefix = "/auth", tags = ["Authentication"])
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hashPassword(password : str)->str:
-  return pwd_context.hash(password)
 
 
 @router.post("/register", status_code = status.HTTP_201_CREATED ,response_model = UserResponse)
@@ -39,5 +37,28 @@ def register_user(userdata : UserCreate, session : Session = Depends(get_session
   return newUser
 
   
+@router.post("/login")
+def login_user( form_data : OAuth2PasswordRequestForm = Depends(),  session : Session = Depends(get_session)):
+
+  user = session.exec(
+    select(User).where((User.username == form_data.username) | (User.email == form_data.username))
+  ).first()
+
+  if not user or not verify_password(form_data.password,user.password):
+    raise HTTPException(
+      status_code = status.HTTP_401_UNAUTHORIZED,
+      detail = "Incorrect username or password",
+      headers = {"WWW-Authenticate" : "Bearer"}
+    )
+  
+  token_payload = {"user_id" : user.id,
+                   "sub" : user.username}
+  
+  access_token = create_access_token(token_payload)
+
+  return {
+    "access_token" : access_token,
+    "token_type" : "bearer"
+  }
 
   
